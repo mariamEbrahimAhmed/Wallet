e3e666700766ad1c05343037c0012476ac34d5e1
mariamibrahem <75806311+mariamibrahem@users.noreply.github.com>
2026-07-06 21:57:27 +0300

    feat: implement custom error classes for better error handling

---
+++ src/errors/AuthorizationError.ts
@@ -0,0 +1,7 @@
+import { AppError } from "./AppError";
+
+export class AuthorizationError extends AppError {
+  constructor(message = "You do not have permission to perform this action") {
+    super(message, 403);
+  }
+}
---
+++ src/errors/index.ts
@@ -0,0 +1,5 @@
+export { AppError } from "./AppError";
+export { NotFoundError } from "./NotFoundError";
+export { BadRequestError } from "./BadRequestError";
+export { AuthenticationError } from "./AuthenticationError";
+export { AuthorizationError } from "./AuthorizationError";

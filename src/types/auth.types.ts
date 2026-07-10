2ea3f3a1f84c86fc8a4123741d588b64fcf946d6
mariamibrahem <75806311+mariamibrahem@users.noreply.github.com>
2026-07-10 23:58:53 +0300

    feat: add LoginResult interface and refresh token generation utility

--- src/types/auth.types.ts
+++ src/types/auth.types.ts
@@ -1,5 +1,13 @@
+import type { User } from "../models";
+
 export interface RegisterUserInput {
   username: string;
   phoneNumber: string;
   password: string;
+}
+
+export interface LoginResult {
+  user: User;
+  accessToken: string;
+  refreshToken: string;
 }
